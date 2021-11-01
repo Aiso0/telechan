8d5b6adc5f600343e65df6d167aa1f0d62f03788
Aiso0 <86371645+Aiso0@users.noreply.github.com>
2021-11-01 20:01:14 +0800

    Update greeting.ts

--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -2,10 +2,10 @@ const replyToMessage = (ctx) =>
   
 
 const greeting = () => (ctx: any) => {
-  
+  const messageId = ctx.message.message_id;
   // const userName = ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
   
-  replyToMessage(ctx);
+  replyToMessage(ctx,messageId);
   // replyToMessage(ctx, messageId, `Hello, ${userName} (user_id: ${ctx.from.id})! \n Your Message id is: ${messageId}`);
 };
 export { greeting };
