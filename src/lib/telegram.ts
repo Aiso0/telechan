79c690e218c75f0e672d10bc6c48a0e88b4b2c5e
Aiso0 <86371645+Aiso0@users.noreply.github.com>
2021-11-01 19:00:07 +0800

    Update telegram.ts

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -1,7 +1,6 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 import Telegraf, { Context as TelegrafContext, Extra } from "telegraf";
 import { ExtraReplyMessage } from "telegraf/typings/telegram-types";
-import { about, greeting, sendkey } from "..";
 import md5 from 'md5';
 import { ok } from "./responses";
 import axios from 'axios';
