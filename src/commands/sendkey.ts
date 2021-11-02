e988863c4d1a4e9ba7bb9485b4f6420b5c877068
Aiso0 <86371645+Aiso0@users.noreply.github.com>
2021-11-02 13:19:11 +0800

    Update sendkey.ts

--- src/commands/sendkey.ts
+++ src/commands/sendkey.ts
@@ -1,7 +1,7 @@
 import md5 from 'md5';
 const VERCEL_URL = process.env.VERCEL_URL;
 const TCKEY = process.env.TCKEY;
-const replyToMessage = (ctx: any, messageId: string,userName,sendkey,site_url) =>
+const replyToMessage = (ctx: any, messageId: string) =>
   ctx.reply( {
     reply_to_message_id: messageId,
   });
@@ -10,11 +10,8 @@ const replyToMessage = (ctx: any, messageId: string,userName,sendkey,site_url) =
 const sendkey = () => (ctx: any) => {
 
   const messageId = ctx.message.message_id;
-  const userName = ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
-  const sendkey = ctx.from.id +'T'+md5(TCKEY+ctx.from.id);
-
-  const site_url = String(VERCEL_URL).split("-")[0]+'.vercel.app';
- replyToMessage(ctx, messageId,userName,sendkey,site_url);
+  
+ replyToMessage(ctx, messageId);
 
   // replyToMessage(ctx, messageId, `Hello, ${userName} (user_id: ${ctx.from.id})! \n Your Message id is: ${messageId}`);
 };
