faffecdc1690189779725094f738a74e890ad0a4
Aiso0 <86371645+Aiso0@users.noreply.github.com>
2021-11-02 13:13:33 +0800

    Update sendkey.ts

--- src/commands/sendkey.ts
+++ src/commands/sendkey.ts
@@ -1,7 +1,10 @@
 import md5 from 'md5';
 const VERCEL_URL = process.env.VERCEL_URL;
 const TCKEY = process.env.TCKEY;
-
+const replyToMessage = (ctx: any, messageId: string) =>
+  ctx.reply(string, {
+    reply_to_message_id: messageId,
+  });
 
 
 const sendkey = () => (ctx: any) => {
@@ -11,7 +14,7 @@ const sendkey = () => (ctx: any) => {
   const sendkey = ctx.from.id +'T'+md5(TCKEY+ctx.from.id);
 
   const site_url = String(VERCEL_URL).split("-")[0]+'.vercel.app';
-
+ replyToMessage(ctx, messageId);
 
   // replyToMessage(ctx, messageId, `Hello, ${userName} (user_id: ${ctx.from.id})! \n Your Message id is: ${messageId}`);
 };
