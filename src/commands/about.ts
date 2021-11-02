d2306ef432119a092c7f13b302ab363723aab04f
Aiso0 <86371645+Aiso0@users.noreply.github.com>
2021-11-02 19:20:14 +0800

    Update about.ts

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -3,10 +3,10 @@ import { author, homepage, name, version } from "../../package.json";
 const debug = require("debug")("bot:about_command");
 
 const about = () => (ctx: any) => {
-	const message = `*${name} ${version}*\n${author}\n${homepage}`;
+	
 	debug(`Triggered "about" command with message \n${message}`);
 
-	return ctx.replyWithMarkdown(message);
+	
 };
 
 export { about };
